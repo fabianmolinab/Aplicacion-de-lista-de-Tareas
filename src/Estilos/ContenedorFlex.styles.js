586a8eeb3aae0d5e47bcf4fbfc1388e520c43032
Fabian Molina <molinafabian11b@gmail.com>
2022-08-13 03:05:12 -0500

    Updates

--- src/Estilos/ContenedorFlex.styles.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const ContenedorFlex = styled.div`
-  display: flex;
-`
--- src/Estilos/Tituloh1.styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-export const Tituloh1 = styled.h1`
-  font-weight: 400;
-  font-size: 24px;
-`
